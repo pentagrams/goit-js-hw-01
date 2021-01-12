d268e73ecf6dd84f631abdc923bbd10bb9becaf0
Max M <maxminullin@gmail.com>
2021-01-12 11:40:13 +0200

    fix1.0

--- js/task-4.js
+++ js/task-4.js
@@ -1,15 +1,16 @@
 const credits = 23580;
 const pricePerDroid = 3000;
+let totalPrice;
+let countDroid = prompt('Какое количество дроидов вы хотите купить? ');
 
-let countDroid = prompt('Какое количество дроидов вы хотите  купить? ');
-let totalPrice = Number(countDroid) * pricePerDroid;
 
 if (countDroid === null) {
     console.log('Отменено пользователем!');
-} else if(credits <= totalPrice) {
-    console.log('Недостаточно средств на счету!')
+} else if((Number(countDroid) * pricePerDroid) > credits){
+   console.log('Недостаточно средств на счету!')
 } else {
     totalPrice = Number(countDroid) * pricePerDroid;
     let balance = credits - totalPrice;
     console.log(`Вы купили ${countDroid} дроидов, на счету осталось ${balance} кредитов.`);
 }
+
--- js/task-5.js
+++ js/task-5.js
@@ -5,21 +5,25 @@ switch (country) {
 
     case 'китай':
         price = '100';
+        alert(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
     case 'чили':
         price = '250';
+        alert(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
     case 'австралия':
         price = '170';
+        alert(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
     case 'индия':
         price = '80';
+        alert(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
     case 'ямайка':
         price = '120';
+        alert(`Доставка в ${country} будет стоить ${price} кредитов`);
         break;
     default:
         alert('В вашей стране доставка не доступна');
 };
 
-alert(`Доставка в ${country} будет стоить ${price} кредитов`);
